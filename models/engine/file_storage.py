3b4f89f3b50205fbb78b4dcb00dfd693d1a060c3
silgenius <olutademartin@gmail.com>
2024-05-14 17:10:35 -0700

    Refactor code to accomodate sterilization and desterilization of user instance

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,9 +64,10 @@ class FileStorage:
             with open(type(self).__file_path, mode="r", encoding="utf-8") as f:
                 file_content = json.loads(f.read())
                 self.__objects = {}
+                module_name = {"BaseModel": "base_model", "User": "user"}
                 for key, value in file_content.items():
                     cls_name, obj_id = key.split(".")
-                    module = __import__("models.base_model", fromlist=[cls_name])
+                    module = __import__("models." + module_name[cls_name], fromlist=[cls_name])
                     cls = getattr(module, cls_name)
                     self.__objects[key] = cls(**value)
         except FileNotFoundError:
