1cb9585222a9236e7ff17df6bf5c92168dbd6727
martin olutade <112440359+silgenius@users.noreply.github.com>
2024-05-19 14:14:54 +0100

    Update test_console.py

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,6 +4,14 @@ from io import StringIO
 from console import HBNBCommand
 from models import storage
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+import json
+
 
 class TestBasicHBNBCommand(unittest.TestCase):
     """ Test Basic HBNB Command """
